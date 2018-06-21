7b6030b5c1b49327e6849e948f09ed0780debbeb
Tobias Bührer <tbuhrer@tobiasb.net>
2018-06-21 19:42:40 +0200

    Custom theme ttobias based on bulma

---
+++ web/themes/custom/ttobias/config.rb
@@ -0,0 +1,20 @@
+# We tell compass that we want to use. SCSS format in this case
+preferred_syntax = :scss
+
+# Where we want our CSS files to go
+css_dir = 'css'
+
+# Where we want to put our source SASS Files
+sass_dir = 'sass'
+
+# While in Dev, we can use this feature to easily debug the CSS styles
+# Production use: = false
+line_comments = true
+
+
+# While in Dev, for readability
+# Production use: output_style = :compressed.
+output_style = :nested
+
+# Production use
+# environment = :production
