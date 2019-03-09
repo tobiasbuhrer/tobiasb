1b2195fedc73db90735a28741869db5a82d1fed6
Tobias Bührer <tbuhrer@tobiasb.net>
2019-03-09 17:39:50 +0100

    minifyhtml 1.7.0

--- web/modules/contrib/minifyhtml/src/EventSubscriber/MinifyHTMLExit.php
+++ web/modules/contrib/minifyhtml/src/EventSubscriber/MinifyHTMLExit.php
@@ -18,49 +18,49 @@ class MinifyHTMLExit implements EventSubscriberInterface {
    *
    * @var string
    */
-  private $content;
+  protected $content;
 
   /**
    * A list of placeholders for HTML elements that won't be minified.
    *
    * @var array
    */
-  private $placeholders = [];
+  protected $placeholders = [];
 
   /**
    * The placeholder token.
    *
    * @var string
    */
-  private $token;
+  protected $token;
 
   /**
    * Config Factory object.
    *
    * @var \Drupal\Core\Config\ConfigFactoryInterface
    */
-  private $config;
+  protected $config;
 
   /**
    * Time object.
    *
    * @var \Drupal\Component\Datetime\TimeInterface
    */
-  private $time;
+  protected $time;
 
   /**
    * Logger Factory object.
    *
    * @var \Drupal\Core\Logger\LoggerChannelFactory
    */
-  private $logger;
+  protected $logger;
 
   /**
    * Abort flag, when dependencies have not been injected.
    *
    * @var bool
    */
-  private $abort = FALSE;
+  protected $abort = FALSE;
 
   /**
    * Constructs a MinifyHTMLExit object.
@@ -72,9 +72,10 @@ class MinifyHTMLExit implements EventSubscriberInterface {
     $arg_error = TRUE;
     if (func_num_args() == 3) {
 
-      // Assigning the arguments this way prevents a signature mismatch exception
-      // that will occur until the cache is cleared to update the service
-      // definition. However, the cache cannot be cleared due to the exception.
+      // Assigning the arguments this way prevents a signature mismatch
+      // exception that will occur until the cache is cleared to update the
+      // service definition. However, the cache cannot be cleared due to the
+      // exception.
       $this->config = func_get_arg(0);
       $this->time = func_get_arg(1);
       $this->logger = func_get_arg(2);
@@ -130,13 +131,13 @@ class MinifyHTMLExit implements EventSubscriberInterface {
   /**
    * Helper function to minify HTML.
    */
-  private function minify() {
+  protected function minify() {
     $callbacks = [
       'minifyhtmlPlaceholderCallbackTextarea' => '/\\s*<textarea(\\b[^>]*?>[\\s\\S]*?<\\/textarea>)\\s*/i',
       'minifyhtmlPlaceholderCallbackPre' => '/\\s*<pre(\\b[^>]*?>[\\s\\S]*?<\\/pre>)\\s*/i',
       'minifyhtmlPlaceholderCallbackIframe' => '/\\s*<iframe(\\b[^>]*?>[\\s\\S]*?<\\/iframe>)\\s*/i',
       'minifyhtmlPlaceholderCallbackScript' => '/\\s*<script(\\b[^>]*?>[\\s\\S]*?<\\/script>)\\s*/i',
-      'minifyhtmlPlaceholderCallbackStyle' => '/\\s*<script(\\b[^>]*?>[\\s\\S]*?<\\/script>)\\s*/i',
+      'minifyhtmlPlaceholderCallbackStyle' => '/\\s*<style(\\b[^>]*?>[\\s\\S]*?<\\/style>)\\s*/i',
     ];
 
     // Only strip HTML comments if required.
@@ -171,7 +172,7 @@ class MinifyHTMLExit implements EventSubscriberInterface {
    * @return string
    *   The content with placeholders.
    */
-  private function minifyhtmlCallback($pattern, $callback) {
+  protected function minifyhtmlCallback($pattern, $callback) {
     $content = preg_replace_callback($pattern, [$this, $callback], $this->content);
 
     if ($error = preg_last_error()) {
@@ -190,7 +191,7 @@ class MinifyHTMLExit implements EventSubscriberInterface {
    * @return string
    *   The placeholder string.
    */
-  private function minifyhtmlPlaceholderCallbackTextarea(array $matches) {
+  protected function minifyhtmlPlaceholderCallbackTextarea(array $matches) {
     return $this->minifyPlaceholderReplace(trim($matches[0]));
   }
 
@@ -203,7 +204,7 @@ class MinifyHTMLExit implements EventSubscriberInterface {
    * @return string
    *   The placeholder string.
    */
-  private function minifyhtmlPlaceholderCallbackPre(array $matches) {
+  protected function minifyhtmlPlaceholderCallbackPre(array $matches) {
     return $this->minifyPlaceholderReplace(trim($matches[0]));
   }
 
@@ -216,7 +217,7 @@ class MinifyHTMLExit implements EventSubscriberInterface {
    * @return string
    *   The placeholder string.
    */
-  private function minifyhtmlPlaceholderCallbackIframe(array $matches) {
+  protected function minifyhtmlPlaceholderCallbackIframe(array $matches) {
     $iframe = preg_replace('/^\\s+|\\s+$/m', '', $matches[0]);
 
     return $this->minifyPlaceholderReplace(trim($iframe));
@@ -231,7 +232,7 @@ class MinifyHTMLExit implements EventSubscriberInterface {
    * @return string
    *   The placeholder string.
    */
-  private function minifyhtmlPlaceholderCallbackScript(array $matches) {
+  protected function minifyhtmlPlaceholderCallbackScript(array $matches) {
     $search = [];
     $replace = [];
 
@@ -263,7 +264,7 @@ class MinifyHTMLExit implements EventSubscriberInterface {
    * @return string
    *   The placeholder string.
    */
-  private function minifyhtmlPlaceholderCallbackStyle(array $matches) {
+  protected function minifyhtmlPlaceholderCallbackStyle(array $matches) {
     $search = [];
     $replace = [];
 
@@ -291,7 +292,7 @@ class MinifyHTMLExit implements EventSubscriberInterface {
    * @return string
    *   The placeholder string.
    */
-  private function minifyPlaceholderReplace($content) {
+  protected function minifyPlaceholderReplace($content) {
     $placeholder = '%' . $this->token . count($this->placeholders) . '%';
     $this->placeholders[$placeholder] = $content;
 
@@ -309,14 +310,14 @@ class MinifyHTMLExit implements EventSubscriberInterface {
    * @return string
    *   String with removed HTML comments.
    */
-  private function minifyhtmlRemoveHtmlComment(array $matches) {
+  protected function minifyhtmlRemoveHtmlComment(array $matches) {
     return (0 === strpos($matches[1], '[') || FALSE !== strpos($matches[1], '<![')) ? $matches[0] : '';
   }
 
   /**
    * Helper function to minify the HTML.
    */
-  private function minifyHtml() {
+  protected function minifyHtml() {
     $search = [];
     $replace = [];
 
