aae83b1000cf103aea6cf1d4b439be485c7f3daa
Tobias Bührer <tbuhrer@tobiasb.net>
2019-03-16 15:17:35 +0100

    updates more_leaflet_markers

--- web/modules/custom/more_leaflet_markers/js/more_leaflet_markers.drupal.js
+++ web/modules/custom/more_leaflet_markers/js/more_leaflet_markers.drupal.js
@@ -3,7 +3,7 @@
  */
 
 (function ($) {
-
+/*
     Drupal.Leaflet.prototype.create_point = function (marker) {
         var latLng = new L.LatLng(marker.lat, marker.lon);
         this.bounds.push(latLng);
@@ -27,31 +27,82 @@
 
         return lMarker;
     };
+*/
 
+    Drupal.Leaflet.prototype.create_point = function (marker) {
+        var self = this;
+        var latLng = new L.LatLng(marker.lat, marker.lon);
+        this.bounds.push(latLng);
+        var lMarker;
+        var tooltip = marker.label ? marker.label.replace(/<[^>]*>/g, '').trim() : '';
+        var options = {
+            title: tooltip
+        };
+
+        if (marker.alt !== undefined) {
+            options.alt = marker.alt;
+        }
+
+        function checkImage(imageSrc, setIcon, logError) {
+            var img = new Image();
+            img.src = imageSrc;
+            img.onload = setIcon;
+            img.onerror = logError;
+        }
+
+        lMarker = new L.Marker(latLng, options);
+
+        if (marker.icon) {
+            checkImage(marker.icon.iconUrl,
+                // Success loading image.
+                function(){
+                    marker.icon.iconSize = marker.icon.iconSize || {};
+                    marker.icon.iconSize.x = marker.icon.iconSize.x || this.naturalWidth;
+                    marker.icon.iconSize.y = marker.icon.iconSize.y || this.naturalHeight;
+                    if (marker.icon.shadowUrl) {
+                        marker.icon.shadowSize = marker.icon.shadowSize || {};
+                        marker.icon.shadowSize.x = marker.icon.shadowSize.x || this.naturalWidth;
+                        marker.icon.shadowSize.y = marker.icon.shadowSize.y || this.naturalHeight;
+                    }
+                    options.icon = self.create_icon(marker.icon);
+                    lMarker.setIcon(options.icon);
+                },
+                // Error loading image.
+                function(err){
+                    console.log("Leaflet: The Icon Image doesn't exist at the requested path: " + marker.icon.iconUrl);
+                });
+        }
+
+        if (marker.targetUrl) {
+            lMarker.on('click', function () {
+                window.location = (marker.targetUrl);
+            });
+        }
+
+        return lMarker;
+    };
 
 
     Drupal.Leaflet.prototype.setMapPosition = function (features) {
+        var self = this;
+
         // Fit Bounds if both them and features exist, and the Map Position in not forced.
-        if (features.length > 0 && !this.settings.map_position_force && this.bounds.length > 0) {
-            this.lMap.fitBounds(new L.LatLngBounds(this.bounds));
+        if (features.length > 0 && !self.settings.map_position_force && self.bounds.length > 0) {
+            self.lMap.fitBounds(new L.LatLngBounds(self.bounds));
 
             // In case of single result use the custom Map Zoom set.
-            if (features.length === 1 && this.settings.zoom) {
-                this.lMap.setZoom(this.settings.zoom);
+            if (features.length === 1 && self.settings.zoom) {
+                self.lMap.setZoom(self.settings.zoom);
             }
 
         }
-        else if (this.settings.map_position_force) {
-            var zoomLevel = this.settings.zoom;
-            if (this.settings.center) {
-                //this.lMap.setZoom(zoomLevel);
-                //var latLngs = new L.LatLng(this.settings.middle.lat, this.settings.middle.lon);
-                //var markerBounds = new L.latLngBounds(latLngs);
-                //this.lMap.map.fitBounds(markerBounds);
-                this.lMap.setView(new L.LatLng(this.settings.center.lat, this.settings.center.lng), zoomLevel);
+        else if (self.settings.map_position_force) {
+            var zoomLevel = self.settings.zoom;
+            if (self.settings.center) {
+                self.lMap.setView(new L.LatLng(self.settings.center.lat, self.settings.center.lng), zoomLevel);
             }
             else {
-                this.lMap.setZoom(zoomLevel);
+                self.lMap.setZoom(zoomLevel);
             }
         }
     };
