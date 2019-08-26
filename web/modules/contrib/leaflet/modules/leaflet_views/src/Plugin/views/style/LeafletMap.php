2a3dda65409671e3c55ecb3af9dd05bbdd42c880
Tobias Bührer <tbuhrer@tobiasb.net>
2019-08-26 09:25:16 +0200

    leaflet 1.19.0

--- web/modules/contrib/leaflet/modules/leaflet_views/src/Plugin/views/style/LeafletMap.php
+++ web/modules/contrib/leaflet/modules/leaflet_views/src/Plugin/views/style/LeafletMap.php
@@ -779,7 +779,6 @@ class LeafletMap extends StylePluginBase implements ContainerFactoryPluginInterf
                 $url = Url::fromRoute('leaflet_views.ajax_popup', $parameters, ['absolute' => TRUE]);
                 $description = sprintf('<div class="leaflet-ajax-popup" data-leaflet-ajax-popup="%s" %s></div>',
                   $url->toString(), LeafletAjaxPopupController::getPopupIdentifierAttribute($entity_type, $entity->id(), $this->options['view_mode'], $langcode));
-                $build_for_bubbleable_metadata['#attached']['library'][] = 'core/drupal.ajax';
                 break;
 
               default:
