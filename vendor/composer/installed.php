61511133796e8374ae48e0620a712959541c7010
Tobias <tobias.buhrer@bluewin.ch>
2026-04-20 18:59:51 +0200

    drupal/core 11.3.8

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '9d7f4d5d22722d9800c766ce241a522210d9f869',
+        'reference' => '12fcdb5bca8daaa994e36be41ec6e2d6a3d9b1d5',
         'name' => 'drupal/recommended-project',
         'dev' => true,
     ),
@@ -236,35 +236,35 @@
             'dev_requirement' => false,
         ),
         'drupal/core' => array(
-            'pretty_version' => '11.3.7',
-            'version' => '11.3.7.0',
+            'pretty_version' => '11.3.8',
+            'version' => '11.3.8.0',
             'type' => 'drupal-core',
             'install_path' => __DIR__ . '/../../web/core',
             'aliases' => array(),
-            'reference' => 'bb36d7d09b0132185bd33be730ec2e6d35c2d627',
+            'reference' => 'd40f45fa436fb089cd54029d2dab387c3040fc2c',
             'dev_requirement' => false,
         ),
         'drupal/core-annotation' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-assertion' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-class-finder' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-composer-scaffold' => array(
-            'pretty_version' => '11.3.7',
-            'version' => '11.3.7.0',
+            'pretty_version' => '11.3.8',
+            'version' => '11.3.8.0',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../drupal/core-composer-scaffold',
             'aliases' => array(),
@@ -274,90 +274,90 @@
         'drupal/core-datetime' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-dependency-injection' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-diff' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-discovery' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-event-dispatcher' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-file-cache' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-file-security' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-filesystem' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-front-matter' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-gettext' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-graph' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-http-foundation' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-php-storage' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-plugin' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-project-message' => array(
-            'pretty_version' => '11.3.7',
-            'version' => '11.3.7.0',
+            'pretty_version' => '11.3.8',
+            'version' => '11.3.8.0',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../drupal/core-project-message',
             'aliases' => array(),
@@ -367,52 +367,52 @@
         'drupal/core-proxy-builder' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-recommended' => array(
-            'pretty_version' => '11.3.7',
-            'version' => '11.3.7.0',
+            'pretty_version' => '11.3.8',
+            'version' => '11.3.8.0',
             'type' => 'metapackage',
             'install_path' => NULL,
             'aliases' => array(),
-            'reference' => 'fb5d6475f4f564d5fa8a8846e4b5e1b97cdfeb94',
+            'reference' => '9c1fa4615a7542504be882eed0c6763b445fb852',
             'dev_requirement' => false,
         ),
         'drupal/core-render' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-serialization' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-transliteration' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-utility' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-uuid' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/core-version' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '11.3.7',
+                0 => '11.3.8',
             ),
         ),
         'drupal/devel' => array(
@@ -565,7 +565,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '9d7f4d5d22722d9800c766ce241a522210d9f869',
+            'reference' => '12fcdb5bca8daaa994e36be41ec6e2d6a3d9b1d5',
             'dev_requirement' => false,
         ),
         'drupal/smtp' => array(
--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '081c7f20057e5d68';
+  public const string VERSIONS_HASH = 'fdcc257477a3ce3e';
 
 }
